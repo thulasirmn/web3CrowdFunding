6e9e509908ac0fc7d6b5b77e76a373f7fd01267f
Thulasi <kaviyab@ilink-systems.com>
2022-12-16 14:27:19 +0530

    initial commit with contract

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract CrowdFunding {
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadlin;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
 }
